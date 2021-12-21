ab2e806314c6ed5c3fb87dd85d1efd37e9c8ca7c
hammad <hammadmehmood@gmail.com>
2021-12-21 18:52:43 +0500

    project restructuring for jetpack compose

--- BuyIt/app/src/main/java/com/airlift/buyit/MainActivity.kt
+++ BuyIt/app/src/main/java/com/airlift/buyit/MainActivity.kt
@@ -1,45 +1,38 @@
 package com.airlift.buyit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.material.composethemeadapter.MdcTheme
+import com.airlift.buyit.ui.theme.BuyItTheme
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-
-        val greeting = findViewById<ComposeView>(R.id.compose_view)
-        greeting.setContent {
-            MdcTheme { // or AppCompatTheme
-                Greeting()
+        setContent {
+            BuyItTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
             }
         }
     }
 }
 
-@Preview
 @Composable
-private fun Greeting() {
-    Text(
-        text = "Greeting",
-        style = MaterialTheme.typography.h5,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = dimensionResource(R.dimen.margin_small))
-            .wrapContentWidth(Alignment.CenterHorizontally)
-    )
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BuyItTheme {
+        Greeting("Android")
+    }
 }
