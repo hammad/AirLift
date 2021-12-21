4eec14b073d7be49b918502d58ad0c2b17be46ed
hammad <hammadmehmood@gmail.com>
2021-12-21 16:29:13 +0500

    jetpack compose added to project with first screen

--- BuyIt/app/src/main/java/com/airlift/buyit/MainActivity.kt
+++ BuyIt/app/src/main/java/com/airlift/buyit/MainActivity.kt
@@ -2,10 +2,44 @@ package com.airlift.buyit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.composethemeadapter.MdcTheme
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val greeting = findViewById<ComposeView>(R.id.compose_view)
+        greeting.setContent {
+            MdcTheme { // or AppCompatTheme
+                Greeting()
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+private fun Greeting() {
+    Text(
+        text = "Greeting",
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
